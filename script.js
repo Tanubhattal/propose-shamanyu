886429bf18f48e8b7d0e1a1578a54a99fe2f8a5c
tanu <anujjatt2018@gmail.com>
2026-02-07 23:01:37 +0530

    Romantic background + proposal options + Tum Se Hi

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let current = 0;
 const pages = document.querySelectorAll(".page");
+const music = document.getElementById("music");
 
 function nextPage() {
   pages[current].classList.remove("active");
@@ -7,4 +8,9 @@ function nextPage() {
   if (current < pages.length) {
     pages[current].classList.add("active");
   }
+}
+
+function playMusic() {
+  music.src =
+    "https://www.youtube.com/embed/mt9xg0mmt28?autoplay=1&loop=1&playlist=mt9xg0mmt28";
 }
